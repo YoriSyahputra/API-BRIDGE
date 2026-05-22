beeddf88147b722708489b8e5eed9cd2427df4c0
Yori Syahputra <122761708+YoriSyahputra@users.noreply.github.com>
2026-05-22 12:51:09 +0700

    Update PrivateKeyScheme.php
    PrivateKeyScheme Add key pass third

--- app/Private/PrivateKeyScheme.php
+++ app/Private/PrivateKeyScheme.php
@@ -5,12 +5,16 @@ class PrivateKeyScheme
 {
     const SHIPPING_KEY1 = 'C/qLUDR6I85iHlEkZLCHvQ==';
     const SHIPPING_KEY2 = 'ex2vK6GQc9QsR011O7UOKA==';
+    const SHIPPING_KEY3 ='pksdms';
 
     public static function getPrivateKey($choose = 'first')
     {
         if($choose === 'second') {
             return self::SHIPPING_KEY2;
+        }elseif($choose === 'third') {
+            return self::SHIPPING_KEY3;
         }
+        
         return self::SHIPPING_KEY1;
     }
 }
