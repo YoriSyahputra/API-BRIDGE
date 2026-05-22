98e314cba2e70c2276d19ed3809fbbec3708048f
Yori Syahputra <122761708+YoriSyahputra@users.noreply.github.com>
2026-05-22 13:15:52 +0700

    Update PrivateKeyScheme.php
    Change Logic to Typed Class Constants

--- app/Private/PrivateKeyScheme.php
+++ app/Private/PrivateKeyScheme.php
@@ -3,18 +3,16 @@ namespace App\Private;
 
 class PrivateKeyScheme
 {
-    const SHIPPING_KEY1 = 'C/qLUDR6I85iHlEkZLCHvQ==';
-    const SHIPPING_KEY2 = 'ex2vK6GQc9QsR011O7UOKA==';
-    const SHIPPING_KEY3 ='pksdms';
+    private const string SHIPPING_KEY1 = 'C/qLUDR6I85iHlEkZLCHvQ==';
+    private const string SHIPPING_KEY2 = 'ex2vK6GQc9QsR011O7UOKA==';
+    private const string SHIPPING_KEY3 ='pksdms';
 
-    public static function getPrivateKey($choose = 'first')
+    public static function getPrivateKey($choose = 'first'): string
     {
-        if($choose === 'second') {
-            return self::SHIPPING_KEY2;
-        }elseif($choose === 'third') {
-            return self::SHIPPING_KEY3;
-        }
-        
-        return self::SHIPPING_KEY1;
+    return match($choose){
+      'first' =>self::SHIPPING_KEY2
+      'second => self::SHIPPING_KEY3
+      'default => self::SHIPPING_KEY1
+      };
     }
 }
